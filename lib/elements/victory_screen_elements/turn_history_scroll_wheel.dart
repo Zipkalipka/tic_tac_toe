e78078c1c51878bfed22df60ed4af3b77d23ad41
Zipkalipka <mikh.ant.evg@gmail.com>
2023-02-03 21:40:10 +0600

    Added Flipping history animation module(now default) based on ListWheelScrollView

--- lib/elements/victory_screen_elements/turn_history_scroll_wheel.dart
+++ lib/elements/victory_screen_elements/turn_history_scroll_wheel.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tic_tac_toe/elements/gameboard_gen.dart';
 import 'package:tic_tac_toe/utilities/game_controller.dart';
-import 'package:tic_tac_toe/models/game_tile.dart';
 import '../../utilities/design_constants.dart';
 
 class HistoryScroll extends StatelessWidget {
@@ -9,122 +9,70 @@ class HistoryScroll extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    FixedExtentScrollController fixedExtentScrollController=FixedExtentScrollController();
+    GameController gameController = Provider.of<GameController>(context);
+    // FixedExtentScrollController fixedExtentScrollController =
+    //     FixedExtentScrollController();
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       width: 350,
       height: 280,
       child: RotatedBox(
         quarterTurns: 1,
-        child: ListWheelScrollView(
-            controller: fixedExtentScrollController,
-            //physics: const BouncingScrollPhysics(),
-            itemExtent: 10,
-            children: wheelGen(context),
-            ),
-      ),
-    );
-  }
-}
+        child: ListWheelScrollView.useDelegate(
+          // perspective: 0.001, //Flip
+          // diameterRatio: 0.3, //Flop
 
-List<Widget> wheelGen(context){
-  GameController gameController =
-  Provider.of<GameController>(context, listen: false);
-  return List<Widget>.generate(gameController.gameTurn, (index) {
-    return RotatedBox(
-      quarterTurns: -1,
-      child: wheelElement(index,context),
-    );
-  }
-  );
-}
+          perspective: 0.001, //SUPER
+          diameterRatio: 0.01,//FLIP
+          squeeze: 0.75,      //FLOP
 
-Widget wheelElement(index, context){
-  GameController gameController =
-  Provider.of<GameController>(context, listen: false);
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      Text(
-        'Turn ${gameController.gameTurn - index}',
-        style: customTextStyle(20),
-      ),
-      Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        width: 300,
-        height: 250,
-        child: HistoryGenerator(
-          turnToRender: index,
+          //controller: fixedExtentScrollController,
+          physics: const FixedExtentScrollPhysics(),
+          itemExtent: 250,
+          childDelegate: ListWheelChildLoopingListDelegate(
+              children: WheelElement.wheelGen(gameController.gameTurn)),
         ),
       ),
-    ],
-  );
+    );
+  }
 }
 
-class HistoryGenerator extends StatelessWidget {
-  final int turnToRender;
+class WheelElement extends StatelessWidget {
+  final int index;
 
-  const HistoryGenerator({Key? key, required this.turnToRender})
-      : super(key: key);
+  const WheelElement({Key? key, required this.index}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     GameController gameController =
         Provider.of<GameController>(context, listen: false);
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: gameController.fieldSize,
-        crossAxisSpacing: 3,
-        mainAxisSpacing: 3,
-      ),
-      padding: EdgeInsets.zero,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: gameController.fieldSize * gameController.fieldSize,
-      itemBuilder: (context, index) {
-        Color iconColor = Colors.white;
-        IconData? gameIcon;
-
-        if (gameController.gameBoard[index].turn ==
-            (gameController.gameTurn - turnToRender - 1)) {
-          iconColor = Colors.orange;
-        }
-
-        if (gameController.gameBoard[index].turn >
-            (gameController.gameTurn - turnToRender - 1)) {
-          gameIcon = null;
-        } else {
-          switch (gameController.gameBoard[index].tileStatus) {
-            case TileStatus.empty:
-              gameIcon = null;
-              break;
-            case TileStatus.cross:
-              gameIcon = Icons.close;
-              break;
-            case TileStatus.circle:
-              gameIcon = Icons.brightness_1_outlined;
-              break;
-          }
-        }
-
-        return Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(5)),
+    return RotatedBox(
+      quarterTurns: -1,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Turn ${gameController.gameTurn - index}',
+            style: customTextStyle(20),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            width: 300,
+            height: 250,
+            child: GameBoard(
+              turnToRender: index,
+              screen: GameScreen.victory,
             ),
-            SizedBox.expand(
-                child: FittedBox(
-                    fit: BoxFit.contain,
-                    child: Icon(
-                      gameIcon,
-                      color: iconColor,
-                    )))
-          ],
-        );
-      },
+          ),
+        ],
+      ),
     );
+  }
+
+  static List<Widget> wheelGen(int gameTurn) {
+    return List<Widget>.generate(gameTurn, (index) {
+      return WheelElement(index: index);
+    });
   }
 }
